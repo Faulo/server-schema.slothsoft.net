f31ae9ba6473e7c9532bd2d7f801985cd4db720e
Daniel Schulz <faulolio@gmail.com>
2018-07-13 15:12:05 +0200

    changed cache, data, and log to be inside vhost root

--- config.php
+++ config.php
@@ -1,5 +1,9 @@
 <?php
-namespace Slothsoft\Farah;
+namespace Slothsoft;
+
+use Slothsoft\Core\ServerEnvironment;
+use Slothsoft\Farah\Dictionary;
+use Slothsoft\Farah\Kernel;
 
 include __DIR__ . '/../../global/slothsoft.core.php';
 include __DIR__ . '/../../global/slothsoft.core.xslt.php';
@@ -7,6 +11,11 @@ include __DIR__ . '/../../global/slothsoft.core.xslt.php';
 include __DIR__ . '/../../global/slothsoft.farah.php';
 //include __DIR__ . '/../../global/slothsoft.minecraft.php';
 //include __DIR__ . '/../../global/slothsoft.mtg.php';
+
+ServerEnvironment::setRootDirectory(__DIR__);
+ServerEnvironment::setCacheDirectory(__DIR__ . DIRECTORY_SEPARATOR . 'cache');
+ServerEnvironment::setLogDirectory(__DIR__ . DIRECTORY_SEPARATOR . 'log');
+ServerEnvironment::setDataDirectory(__DIR__ . DIRECTORY_SEPARATOR . 'data');
 
 Kernel::setCurrentSitemap('farah://slothsoft@schema.slothsoft.net/sitemap');
 Dictionary::setSupportedLanguages('en-us');
