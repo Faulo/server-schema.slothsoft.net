4964165bd6989fdcdc60631a874fe094f134eecb
Daniel Schulz <faulolio@gmail.com>
2018-10-18 16:49:55 +0200

    new asset url

--- public/index.php
+++ public/index.php
@@ -2,15 +2,19 @@
 
 use Slothsoft\Farah\Kernel;
 use Slothsoft\Farah\Http\MessageFactory;
+use Slothsoft\Farah\RequestStrategy\LookupAssetStrategy;
 use Slothsoft\Farah\RequestStrategy\LookupPageStrategy;
 use Slothsoft\Farah\ResponseStrategy\SendHeaderAndBodyStrategy;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$requestStrategy = new LookupPageStrategy();
-$responseStrategy = new SendHeaderAndBodyStrategy();
-
 $request = MessageFactory::createServerRequest($_SERVER, $_REQUEST, $_FILES);
+if (preg_match('~^/[^/]+@[^/]+~', $request->getUri()->getPath())) {
+    $requestStrategy = new LookupAssetStrategy();
+} else {
+    $requestStrategy = new LookupPageStrategy();
+}
+$responseStrategy = new SendHeaderAndBodyStrategy();
 
 $kernel = Kernel::getInstance();
 $kernel->handle($requestStrategy, $responseStrategy, $request);
