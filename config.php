9f764a4402d5d99528b6898fe8bd66de7ead3f50
Daniel Schulz <faulolio@gmail.com>
2019-11-17 16:35:23 +0100

    enable tracking

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ use Slothsoft\Farah\Kernel;
 
 include __DIR__ . '/../../global/slothsoft.core.php';
 include __DIR__ . '/../../global/slothsoft.core.xslt.php';
-//include __DIR__ . '/../../global/slothsoft.core.dbms.php';
+include __DIR__ . '/../../global/slothsoft.core.dbms.php';
 include __DIR__ . '/../../global/slothsoft.farah.php';
 //include __DIR__ . '/../../global/slothsoft.minecraft.php';
 //include __DIR__ . '/../../global/slothsoft.mtg.php';
@@ -18,4 +18,5 @@ ServerEnvironment::setLogDirectory(__DIR__ . DIRECTORY_SEPARATOR . 'log');
 ServerEnvironment::setDataDirectory(__DIR__ . DIRECTORY_SEPARATOR . 'data');
 
 Kernel::setCurrentSitemap('farah://slothsoft@schema.slothsoft.net/sitemap');
+Kernel::setTrackingEnabled(true);
 Dictionary::setSupportedLanguages('en-us');
