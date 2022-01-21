cd502d8ac575b34570f17c0b9e36d295a9ff7cb7
Daniel Schulz <faulolio@gmail.com>
2022-01-21 15:43:35 +0100

    Ignore failed includes

--- config.php
+++ config.php
@@ -5,12 +5,12 @@ use Slothsoft\Core\ServerEnvironment;
 use Slothsoft\Farah\Dictionary;
 use Slothsoft\Farah\Kernel;
 
-include __DIR__ . '/../../global/slothsoft.core.php';
-include __DIR__ . '/../../global/slothsoft.core.xslt.php';
-include __DIR__ . '/../../global/slothsoft.core.dbms.php';
-include __DIR__ . '/../../global/slothsoft.farah.php';
-//include __DIR__ . '/../../global/slothsoft.minecraft.php';
-//include __DIR__ . '/../../global/slothsoft.mtg.php';
+@include __DIR__ . '/../../global/slothsoft.core.php';
+@include __DIR__ . '/../../global/slothsoft.core.xslt.php';
+@include __DIR__ . '/../../global/slothsoft.core.dbms.php';
+@include __DIR__ . '/../../global/slothsoft.farah.php';
+// @include __DIR__ . '/../../global/slothsoft.minecraft.php';
+// @include __DIR__ . '/../../global/slothsoft.mtg.php';
 
 ServerEnvironment::setRootDirectory(__DIR__);
 ServerEnvironment::setCacheDirectory(__DIR__ . DIRECTORY_SEPARATOR . 'cache');
