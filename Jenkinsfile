bd7e724a0abcbcfd48cf4336b59f6748bae08c64
Daniel Schulz <faulolio@gmail.com>
2024-09-24 17:42:33 +0200

    build: docker

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,4 +1,36 @@
-farahServerPipeline {
-    VHOST_NAME = 'schema.slothsoft.net'
-    PHP_VERSION = '7.4'
+pipeline {
+	agent {
+	    label 'jenkins && docker'
+	}
+    stages {
+        stage('Load environment') {
+            steps {
+                script {
+					withEnv(readFile('.env').split('\n') as List) {
+						stage ('Pull image') {
+							sh "docker image pull faulo/farah:$PHP_VERSION"
+						}
+						stage ('Run tests') {
+							docker.image("faulo/farah:$PHP_VERSION").inside {
+								sh 'composer update --no-interaction'
+								sh 'composer exec phpunit -- --log-junit report.xml'
+								junit 'report.xml'
+								stash name:'lock', includes:'composer.lock'
+							}
+						}
+						stage ('Deploy stack') {
+							dir("/var/vhosts/$STACK_NAME") {
+								checkout scm
+								def service = env.STACK_NAME + '_' + env.STACK_NAME
+								unstash 'lock'
+								sh "docker stack deploy $STACK_NAME --detach=true --prune --resolve-image=never -c=docker-compose.yml"
+								sh "docker service update --force " + service
+								//sh 'docker exec $(docker ps -q -f name=' + service + ') composer install --no-interaction --no-dev'
+							}
+						}
+					}
+                }
+            }
+        }
+	}
 }
--- scripts/bootstrap.php
+++ scripts/bootstrap.php
@@ -1,7 +1,29 @@
 <?php
 declare(strict_types = 1);
 
+namespace Slothsoft;
+
 use Slothsoft\Farah\Module\Module;
+use Slothsoft\Core\ServerEnvironment;
+use Slothsoft\Farah\Dictionary;
+use Slothsoft\Farah\Kernel;
 
-Module::registerWithXmlManifestAndDefaultAssets('slothsoft@schema.slothsoft.net', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'assets');
+@include __DIR__ . '/../../global/slothsoft.core.php';
+@include __DIR__ . '/../../global/slothsoft.core.xslt.php';
+@include __DIR__ . '/../../global/slothsoft.core.dbms.php';
+@include __DIR__ . '/../../global/slothsoft.farah.php';
+// @include __DIR__ . '/../../global/slothsoft.minecraft.php';
+// @include __DIR__ . '/../../global/slothsoft.mtg.php';
 
+$root = dirname(__DIR__);
+
+ServerEnvironment::setRootDirectory($root);
+ServerEnvironment::setCacheDirectory($root . DIRECTORY_SEPARATOR . 'cache');
+ServerEnvironment::setLogDirectory($root . DIRECTORY_SEPARATOR . 'log');
+ServerEnvironment::setDataDirectory($root . DIRECTORY_SEPARATOR . 'data');
+
+Kernel::setCurrentSitemap('farah://slothsoft@schema.slothsoft.net/sitemap');
+Kernel::setTrackingEnabled(true);
+Dictionary::setSupportedLanguages('en-us');
+
+Module::registerWithXmlManifestAndDefaultAssets('slothsoft@schema.slothsoft.net', $root . DIRECTORY_SEPARATOR . 'assets');
