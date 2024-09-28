88f590b1c00cfb2f90e319378cb653ac19195764
Daniel Schulz <faulolio@gmail.com>
2024-09-28 22:41:15 +0200

    build: update files

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,17 +1,16 @@
 <?php
 declare(strict_types = 1);
+namespace Slothsoft\Server\Schema;
 
-use Slothsoft\Farah\Module\Module;
 use Slothsoft\Core\ServerEnvironment;
 use Slothsoft\Farah\Dictionary;
 use Slothsoft\Farah\Kernel;
+use Slothsoft\Farah\Module\Module;
 
 @include __DIR__ . '/../../global/slothsoft.core.php';
 @include __DIR__ . '/../../global/slothsoft.core.xslt.php';
 @include __DIR__ . '/../../global/slothsoft.core.dbms.php';
 @include __DIR__ . '/../../global/slothsoft.farah.php';
-// @include __DIR__ . '/../../global/slothsoft.minecraft.php';
-// @include __DIR__ . '/../../global/slothsoft.mtg.php';
 
 $root = dirname(__DIR__);
 
@@ -20,8 +19,8 @@ ServerEnvironment::setCacheDirectory($root . DIRECTORY_SEPARATOR . 'cache');
 ServerEnvironment::setLogDirectory($root . DIRECTORY_SEPARATOR . 'log');
 ServerEnvironment::setDataDirectory($root . DIRECTORY_SEPARATOR . 'data');
 
-Kernel::setCurrentSitemap('farah://slothsoft@schema.slothsoft.net/sitemap');
-Kernel::setTrackingEnabled(true);
+Kernel::setCurrentSitemap('farah://slothsoft/schema.slothsoft.net/sitemap');
+Kernel::setTrackingEnabled(false);
 Dictionary::setSupportedLanguages('en-us');
 
 Module::registerWithXmlManifestAndDefaultAssets('slothsoft@schema.slothsoft.net', $root . DIRECTORY_SEPARATOR . 'assets');
