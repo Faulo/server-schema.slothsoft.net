d7ddd9dfaa2df58a47edb03c581f4b49159830d1
Daniel Schulz <faulolio@gmail.com>
2025-06-23 17:47:01 +0200

    build: update files

--- .jenkins/docker-compose-debug.bat
+++ .jenkins/docker-compose-debug.bat
@@ -1,5 +1,5 @@
 setlocal
 cd %~dp0
 call load-env
-start "%SERVER_NAME% (localhost)" docker compose --file docker-compose-debug.yml up --build
+start "%SERVER_NAME% (localhost)" docker compose --project-name %STACK_NAME% --file docker-compose-debug.yml up --build
 endlocal
--- .jenkins/podman-compose-debug.bat
+++ .jenkins/podman-compose-debug.bat
@@ -1,5 +1,5 @@
 setlocal
 cd %~dp0
 FOR /F "tokens=*" %%i IN ('type ..\.env') DO SET %%i
-start "%SERVER_NAME% (localhost)" podman compose --file podman-compose-debug.yml up --build
+start "%SERVER_NAME% (localhost)" podman compose --project-name %STACK_NAME% --file podman-compose-debug.yml up --build
 endlocal
