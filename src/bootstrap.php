516714b08e0ecd85afaa7ac73a8803c4652b625b
Daniel Schulz (Estelle) <faulolio@gmail.com>
2025-11-19 20:37:24 +0100

    refactor(dictionary): disable v0

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -8,6 +8,7 @@ use Slothsoft\Core\DBMS\Client;
 use Slothsoft\Farah\Dictionary;
 use Slothsoft\Farah\Kernel;
 use Slothsoft\Farah\Module\Module;
+use Slothsoft\Farah\Module\Executable\ResultBuilderStrategy\TransformationResultBuilder;
 $root = dirname(__DIR__);
 
 ServerEnvironment::setRootDirectory($root);
@@ -24,3 +25,5 @@ Module::registerWithXmlManifestAndDefaultAssets('slothsoft@schema.slothsoft.net'
 if ($file = getenv('MYSQL_ROOT_PASSWORD_FILE') and $password = file_get_contents($file)) {
     Client::setDefaultAuthority(new Authority('mysql', 'root', $password));
 }
+
+TransformationResultBuilder::$translateDictionaryAlpha = false;
