1b4e606df759b9ae5f168b14c74681e7452177f0
Daniel Schulz (Estelle) <faulolio@gmail.com>
2026-01-06 19:09:03 +0100

    build: update files

--- tests/IndexTest.php
+++ tests/IndexTest.php
@@ -2,26 +2,25 @@
 declare(strict_types = 1);
 namespace Slothsoft\Server\Schema\Tests;
 
-use Slothsoft\Farah\Http\MessageFactory;
 use Slothsoft\FarahTesting\Module\AbstractTestCase;
+use Slothsoft\Farah\Http\MessageFactory;
 use Slothsoft\Farah\RequestStrategy\LookupPageStrategy;
 use DOMDocument;
 
-class IndexTest extends AbstractTestCase {
-
+final class IndexTest extends AbstractTestCase {
+    
     public function testIndex() {
         $_SERVER['REQUEST_URI'] = '/';
-
+        
         $requestStrategy = new LookupPageStrategy();
-
+        
         $request = MessageFactory::createServerRequest($_SERVER, $_REQUEST, $_FILES);
-
+        
         $response = $requestStrategy->process($request);
-
-        $data = stream_get_contents($response->getBody()->detach());
-
+        
+        $data = (string) $response->getBody();
+        
         $doc = new DOMDocument();
         $this->assertTrue($doc->loadXML($data));
     }
 }
-
