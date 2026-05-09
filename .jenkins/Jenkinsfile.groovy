c321ee235ab33269219d3917cd6addf873c43a17
Daniel Schulz <faulolio@gmail.com>
2026-05-09 20:53:47 +0200

    build: update files

--- .jenkins/Jenkinsfile.groovy
+++ .jenkins/Jenkinsfile.groovy
@@ -1,39 +1,32 @@
 pipeline {
-	agent {
-		label 'mörkö'
-	}
-	options {
-		disableConcurrentBuilds()
-		disableResume()
-	}
-	stages {
-		stage('Load environment') {
-			steps {
-				script {
-					withEnv(readFile('.env').split('\n') as List) {
-						stage('Setup dependencies') {
-							def dockerTool = tool(type: 'dockerTool', name: 'Default') + "/bin/docker"
+    agent {
+        label 'mörkö'
+    }
+    options {
+        disableConcurrentBuilds()
+        disableResume()
+        disableRestartFromStage()
+    }
+    stages {
+        stage('Load environment') {
+            steps {
+                script {
+                    withEnvFile {
+                        stage ('Run tests') {
+                            docker.image("faulo/farah:${PHP_VERSION}").inside('-v /var/vhosts/${SERVER_NAME}:${WORKSPACE}/data') {
+                                callShell 'composer install --no-interaction --optimize-autoloader --classmap-authoritative'
+                                callShell 'composer exec server-clean cache logs'
 
-							callShell "${dockerTool} pull faulo/farah:${PHP_VERSION}"
+                                catchError(buildResult: 'UNSTABLE', catchInterruptions: false) {
+                                    callShell 'composer exec phpunit -- --log-junit report.xml'
+                                }
 
-							env.DOCKER_OS_TYPE = callShellStdout "${dockerTool} info --format '{{.OSType}}'"
-							env.DOCKER_WORKDIR = callShellStdout "${dockerTool} image inspect faulo/farah:${PHP_VERSION} --format '{{.Config.WorkingDir}}'"
-						}
-						stage ('Run tests') {
-							withDockerContainer(image: "faulo/farah:${PHP_VERSION}", toolName: 'Default', args: '-v /var/vhosts/schema.slothsoft.net:$WORKSPACE/data') {
-								callShell 'composer install --no-interaction --optimize-autoloader --classmap-authoritative'
-								callShell 'composer exec server-clean cache logs'
-
-								catchError(buildResult: 'UNSTABLE', catchInterruptions: false) {
-									callShell 'composer exec phpunit -- --log-junit report.xml'
-								}
-
-								junit 'report.xml'
-							}
-						}
-					}
-				}
-			}
-		}
-	}
+                                junit 'report.xml'
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
